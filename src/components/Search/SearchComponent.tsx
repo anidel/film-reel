fcf087edf0ff16c509da85694ca3c3154c55db4a
Aniello Del Sorbo <anidel@gmail.com>
2018-09-24 15:38:54 +0200

    a slight improvment to the search input

--- src/components/Search/SearchComponent.tsx
+++ src/components/Search/SearchComponent.tsx
@@ -7,13 +7,37 @@ export interface ISearchProps {
   onSearch(searchText: string): Action;
 }
 
-const Input = styled.input``;
+const SearchLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin: 20px;
+  width: 50%;
+`;
+
+const Label = styled.label`
+  margin-bottom: 10px;
+`;
+
+const Input = styled.input`
+  width: 100%;
+`;
 
 export class SearchComponent extends React.PureComponent<ISearchProps> {
   public render() {
     const { searchText } = this.props;
 
-    return <Input type="text" value={searchText} onChange={this.onChange} />;
+    return (
+      <SearchLayout>
+        <Label htmlFor="search_text">Search for a movie:</Label>
+        <Input
+          id="search_text"
+          type="text"
+          placeholder={"Enter a query here"}
+          value={searchText}
+          onChange={this.onChange}
+        />
+      </SearchLayout>
+    );
   }
 
   private onChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
